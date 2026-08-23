46c986df54337d1f7df48d35cd0066c576a7ea84
Monu nayan <134795470+monunayan@users.noreply.github.com>
2026-08-23 13:42:33 +0530

    Time: 1 ms (99.98%) | Memory: 42.4 MB (91.09%) - LeetSync

--- 7-reverse-integer/reverse-integer.java
+++ 7-reverse-integer/reverse-integer.java
@@ -8,19 +8,13 @@ class Solution {
             int digit = x % 10;
             x /= 10;
 
-            if (rev > Integer.MAX_VALUE / 10 ||
-                (rev == Integer.MAX_VALUE / 10 && digit > 7)) {
-                return 0;
-            }
-
-            if (rev < Integer.MIN_VALUE / 10 ||
-                (rev == Integer.MIN_VALUE / 10 && digit < -8)) {
-                return 0;
-            }
-
-            rev = rev * 10 + digit;
+           
+        
+        if(rev>Integer.MAX_VALUE/10 || rev<Integer.MIN_VALUE/10){
+          return 0;
         }
-
+         rev = rev*10+digit;
+        }
         return rev;
     }
 }
