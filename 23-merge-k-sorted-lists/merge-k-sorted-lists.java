cd4a90933ab9342da4986f72fb71a147e8a670e0
Monu nayan <134795470+monunayan@users.noreply.github.com>
2026-08-27 20:22:04 +0530

    Time: 4 ms (79.62%) | Memory: 46.8 MB (64.50%) - LeetSync

---
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -0,0 +1,31 @@
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+
+        if (lists == null || lists.length == 0)
+            return null;
+
+        PriorityQueue<ListNode> pq =
+            new PriorityQueue<>((a, b) -> a.val - b.val);
+
+        for (ListNode node : lists) {
+            if (node != null)
+                pq.offer(node);
+        }
+
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+
+        while (!pq.isEmpty()) {
+
+            ListNode node = pq.poll();
+
+            curr.next = node;
+            curr = node;
+
+            if (node.next != null)
+                pq.offer(node.next);
+        }
+
+        return dummy.next;
+    }
+}
