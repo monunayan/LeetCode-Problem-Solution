3d50306875ce664e33b40cb90ad3a6e1bd717d79
Monu nayan <134795470+monunayan@users.noreply.github.com>
2026-09-22 22:42:46 +0530

    Time: 1 ms (82.23%) | Memory: 43.2 MB (49.14%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -1,32 +1,15 @@
 class Solution {
     public int numDecodings(String s) {
-
-        int n = s.length();
-
-        int[] dp = new int[n + 1];
-
-        dp[0] = 1;
-
-        // First character
-        if (s.charAt(0) != '0') {
-            dp[1] = 1;
-        }
-
-        for (int i = 2; i <= n; i++) {
-
-            // One digit: s[i-1]
-            if (s.charAt(i - 1) != '0') {
-                dp[i] += dp[i - 1];
+        int n=s.length() ;
+        int[] dp=new int[n+1] ;
+        dp[n]=1 ;
+        for(int i = n-1 ; i >= 0 ; i--)
+            if(s.charAt(i)!='0') 
+            {
+                dp[i] = dp[i+1] ;
+                if(i < n-1 && (s.charAt(i)=='1' || s.charAt(i)=='2' && s.charAt(i+1)<'7')) 
+				    dp[i]+=dp[i+2];
             }
-
-            // Two digits: s[i-2] + s[i-1]
-            int num = Integer.parseInt(s.substring(i - 2, i));
-
-            if (num >= 10 && num <= 26) {
-                dp[i] += dp[i - 2];
-            }
-        }
-
-        return dp[n];
+        return dp[0];   
     }
 }
