65b11a969a124077940f61caa639347bdcb1db42
Monu nayan <134795470+monunayan@users.noreply.github.com>
2026-09-23 13:31:36 +0530

    Time: 7 ms (23.50%) | Memory: 43.3 MB (51.20%) - LeetSync

---
+++ 97-interleaving-string/interleaving-string.java
@@ -0,0 +1,45 @@
+class Solution {
+
+    public boolean isInterleave(String s1, String s2, String s3) {
+
+        int n = s1.length();
+        int m = s2.length();
+
+        // Length must match
+        if (n + m != s3.length()) {
+            return false;
+        }
+
+        boolean[][] dp = new boolean[n + 1][m + 1];
+
+        dp[0][0] = true;
+
+        for (int i = 0; i <= n; i++) {
+
+            for (int j = 0; j <= m; j++) {
+
+                if (i == 0 && j == 0) {
+                    continue;
+                }
+
+                // Take character from s1
+                if (i > 0 &&
+                    s1.charAt(i - 1) == s3.charAt(i + j - 1) &&
+                    dp[i - 1][j]) {
+
+                    dp[i][j] = true;
+                }
+
+                // Take character from s2
+                if (j > 0 &&
+                    s2.charAt(j - 1) == s3.charAt(i + j - 1) &&
+                    dp[i][j - 1]) {
+
+                    dp[i][j] = true;
+                }
+            }
+        }
+
+        return dp[n][m];
+    }
+}
